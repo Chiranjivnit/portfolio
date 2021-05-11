6bdcaca7e4bb00f5a58bdca28a18c45b994461bc
Chiranjivnit <chiranjiv.nit@gmail.com>
2021-05-11 09:37:36 +0530

    Updates

---
+++ precache-manifest.575c8001dc8b34c72f64e49c11c4ab50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c720a7df7f1577ee107e271f9f789f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8bec57c86f5f62eebf56",
+    "url": "/portfolio/static/css/main.4bfa2c07.chunk.css"
+  },
+  {
+    "revision": "91bd4f4b1f0c2ffde4fe",
+    "url": "/portfolio/static/js/2.b4ec782b.chunk.js"
+  },
+  {
+    "revision": "7540bdb98473f45f33f933c9427c257d",
+    "url": "/portfolio/static/js/2.b4ec782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bec57c86f5f62eebf56",
+    "url": "/portfolio/static/js/main.1070a93c.chunk.js"
+  },
+  {
+    "revision": "7cc693fd4d3758c648be",
+    "url": "/portfolio/static/js/runtime-main.7b3663b8.js"
+  },
+  {
+    "revision": "490957d89fae5c937da7013f11dbc85e",
+    "url": "/portfolio/static/media/ChiranjivResume.490957d8.pdf"
+  },
+  {
+    "revision": "e28604a08c739b00e2a5d6946fc315c7",
+    "url": "/portfolio/static/media/fullstack.e28604a0.png"
+  }
+]);
