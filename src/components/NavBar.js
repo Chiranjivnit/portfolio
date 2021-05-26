cf37a84e3f19cbdc1a6fdf2b23f2e16512120b00
chiranjivnit <chiranjiv.nit@gmail.com>
2021-05-26 15:04:17 +0530

    responsive update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,6 +15,11 @@ function NavBar({ aboutPage,contactPage }) {
                 <div className="leftlogo">
                     <h1>Chiranjib Rout</h1>
                 </div>
+                <div className="toggleBtn" >
+                 <span></span>
+                 <span></span>
+                 <span></span>
+                </div>
                 <div className="rightMenu">
                     <NavLink className={aboutPage || contactPage ? "" : "home"} to="/" ><a>Home</a></NavLink>
                     <a href="https://github.com/Chiranjivnit?tab=repositories" target="_blank">Projects</a>
